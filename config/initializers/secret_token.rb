f1084d0a7e6227b4390b31c31ac4fd77980a6f0b
Weston <weston.wei@sidways.com>
2012-07-25 10:18:08 +0800

    Init Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuddyCom::Application.config.secret_token = '260791503e45eea09a244e05d210baf0c4e15a6b4fb20d0c3eb6f9d0e3a4392c587ced1fbe983731e386b100a1def60aaa0a39c824c4e0fba2f046452b7457c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BuddyCom::Application.config.session_store :cookie_store, key: '_buddy.com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BuddyCom::Application.config.session_store :active_record_store
